fa5e0f3273a126a3db61a8f60fa960dabf231ca7
Nirupam Das <nirupam.saie@gmail.com>
2020-10-03 01:13:59 +0530

    Security Patch - Name extracted from user-service, feign client calls secured api

--- gateway/src/main/java/com/innova/doctrro/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/innova/doctrro/gateway/GatewayApplication.java
@@ -3,22 +3,13 @@ package com.innova.doctrro.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @GetMapping("/asdf")
-    public String hello(Authentication auth) {
-        System.out.println(auth.getAuthorities());
-        return "hello";
-    }
 }
