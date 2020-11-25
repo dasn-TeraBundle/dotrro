64c5bbff107f5573e29b0f9d72152a1c5ef58342
Nirupam Das <nirupam.saie@gmail.com>
2020-11-25 21:37:33 +0530

    Cleanup - Old slot generation code

--- search-service/src/main/java/com/innova/doctrro/ss/controller/SearchController.java
+++ search-service/src/main/java/com/innova/doctrro/ss/controller/SearchController.java
@@ -7,7 +7,6 @@ import com.innova.doctrro.ss.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/search-service")
@@ -26,12 +25,6 @@ public class SearchController {
                                             @RequestParam(value = "radius") int radius) {
         return searchService.search(latitude, longitude, radius);
     }
-
-//    @GetMapping("/facility/{fid}/doctor/{regId}")
-//    public Mono<FacilityDtoResponse> getAllSlots(@PathVariable String fid,
-//                                                 @PathVariable String regId) {
-//        return searchService.findAllBookingSlots(fid, regId);
-//    }
 
     @GetMapping("/doctor")
     public Flux<DoctorDtoResponse> search(@RequestParam(value = "lat") double latitude,
