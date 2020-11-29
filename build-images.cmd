8c690310b9efd40487eefab5311abaab739a9502
Nirupam Das <nirupam.saie@gmail.com>
2020-11-29 19:34:32 +0530

    Eureka, config, gateway and user-service docker files added

---
+++ build-images.cmd
@@ -0,0 +1,19 @@
+rem mvn clean package -DskipTests=true
+
+rem mvn clean package -DskipTests=true
+
+cd eureka
+docker build -t eureka .
+cd ..
+
+cd config-server
+docker build -t config-server .
+cd ..
+
+cd gateway
+docker build -t gateway .
+cd ..
+
+cd user-service
+docker build -t user-service .
+cd ..
---
+++ cleanup-images.cmd
@@ -0,0 +1,4 @@
+docker rmi eureka
+docker rmi gateway
+docker rmi config-server
+docker rmi user-service
